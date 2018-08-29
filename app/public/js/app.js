f520e8571846938a378c1aaa5f919d84bc038aa6
kolbert <kolbert@yandex.ru>
2018-08-29 17:17:58 +0300

    nothing important

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -6,3 +6,22 @@ console.log('Привет из бабеля и файла JS для фронта
 document.getElementById('mobileMenuCheckbox').onclick = function () {
     document.querySelector('body').style.overflow = this.checked ? 'hidden' : 'auto';
 };
+
+var menuContainer = document.getElementById('menu__container');
+
+menuContainer.addEventListener('mouseover', function () {
+
+    // document.getElementsByClassName('menu__link')[0].style.color = '#0000';
+
+    if (event.target.tagName !== 'A') {
+        console.log('ghjk');
+        event.target.style.color = '#5555';
+    }
+});
+
+menuContainer.addEventListener('mouseout', function () {
+    if (event.target.tagName !== 'A') {
+        console.log('ghjk');
+        event.target.style.color = '#5555';
+    }
+});
