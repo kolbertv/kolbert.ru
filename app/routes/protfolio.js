1a4a5c5f0bc6a780558a38a114c76cee50f8512a
kolbert <kolbert@yandex.ru>
2019-01-10 00:44:56 +0300

    123

--- app/routes/protfolio.js
+++ app/routes/protfolio.js
@@ -6,10 +6,17 @@ const rootDir = require('../util/path')
 
 const router = express.Router();
 
-router.get("/portfolio", (req, res, next) => {
+router.get("/portfolio/:id", (req, res, next) => {
+
+    let yearId = req.params.id === 'exampl' ? 'Примеры выполненных работ' : `Работы выполненные за ${req.params.id} год`;
+
     res.render('portfolio', {
         title: '- перечень выполненных работ',
-        path: '/portfolio'
+        path: '/portfolio',
+        yearId: yearId,
+        work: [{
+            title: 'Страница находится в разработке'
+        }]
     });
 });
 
