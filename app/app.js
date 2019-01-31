68745527bc94add810ed2601be097d87351630df
kolbert vladimir <kolbert@yandex.ru>
2019-01-31 16:59:11 +0300

    123

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const config = require("./lib/config");
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
--- app/models/user.js
+++ app/models/user.js
@@ -1,36 +1,24 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 const ObjectId = mongodb.ObjectID;
 
-const getDb = require('../util/database').getDb;
-
+const getDb = require("../util/database").getDb;
 
 class User {
-    constructor(username, email) {
-        this.name = username;
-        this.email = email;
-        this.createdAt = new Date();
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+    this.createdAt = new Date();
+  }
 
-    }
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
 
-    save() {
-        const db = getDb();
-
-        return db.collection('users').insertOne(this);
-    }
-
-    static findUserById(userId) {
-        const db = getDb();
-        return db.collection('users')
-            .findOne({
-                _id: new ObjectId(userId)
-            })
-            .then(user => user)
-            .catch(err => console.log(err));
-    }
+  static findById(userId) {
+    const db = getDb();
+    return db.collection("users").findOne({ _id: new ObjectId(userId) });
+  }
 }
-
-
-
-
 
 module.exports = User;
