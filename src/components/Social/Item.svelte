1e2cdd080127b253125e7f5886309bfcc8c8ecfa
kolbert vladimir <kolbert@yandex.ru>
2019-07-29 17:02:59 +0300

    add social row amd item to footer

---
+++ src/components/Social/Item.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let link;
+  export let icon;
+</script>
+
+<style type="text/sass">
+  a {
+    padding: 0 8px;
+  }
+  span {
+    font-size: 24px;
+    color: gray;
+    &:hover {
+      color: dimgray;
+    }
+  }
+</style>
+
+<a href={link}>
+  <span>
+    <i class={icon} />
+  </span>
+</a>
---
+++ src/components/Social/Social.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Item from "./Item.svelte";
+</script>
+
+<div>
+  <Item
+    icon={'icon-facebook-squared'}
+    link={'https://www.facebook.com/kolbertv'} />
+  <Item
+    icon={'icon-linkedin-squared'}
+    link={'https://www.linkedin.com/in/kolbert'} />
+  <Item
+    icon={'icon-youtube-play'}
+    link={'https://www.youtube.com/user/vkolbert'} />
+  <Item icon={'icon-vkontakte'} link={'https://vk.com/kolbert'} />
+
+</div>
