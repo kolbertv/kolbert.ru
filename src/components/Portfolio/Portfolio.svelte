558c0c663c19810f01fc7405eaa6db509cd7eff5
vladimir <kolbert@yandex.ru>
2019-08-05 23:29:59 +0300

    add construction info

--- src/components/Portfolio/Portfolio.svelte
+++ src/components/Portfolio/Portfolio.svelte
@@ -1,9 +1,11 @@
 <style>
-
+    p {
+        text-transform: uppercase;
+        text-align: center;
+    }
 
 </style>
 
 <div class="container">
-
-
+    <p>Данный раздел находится в разработке</p>
 </div>
