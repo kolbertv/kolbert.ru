cc3f572bf70c6b27c9834137810366c97dfcf076
kolbert vladimir <kolbert@yandex.ru>
2019-08-16 13:50:16 +0300

    add jwt

--- src/server.js
+++ src/server.js
@@ -3,14 +3,38 @@ import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
+// import bodyParser from 'body-parser';
+// import session from 'express-session';
+// import sessionFileStore from 'session-file-store';
+
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
+
+// const FileStore = sessionFileStore(session);
+
 polka() // You can also use Express
+	// .use(bodyParser.json())
+	// .use(session({
+	// 	secret:'secret',
+	// 	resave: false,
+	// 	saveUninitialized: true,
+	// 	cookie: {
+	// 		maxAge: 1000000000
+	// 	},
+	// 	store: new FileStore({
+	// 		path:process.env.NOW ? '/tmp/session' : '.session'
+	// 	})
+
+	// }))
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
-		sapper.middleware()
+		sapper.middleware({
+		// 	session: req => ({
+		// 		user: req.session && req.session.user
+		// 	})
+		})
 	)
 	.listen(PORT, err => {
 		if (err) console.log('error', err);
